327dce5433f8298fb7bfc0faae678812ae52f200
Denys <denafen2000@gmail.com>
2022-05-20 19:34:05 +0300

    Add Number validation

--- src/data/dto/create-short-term-data.dto.ts
+++ src/data/dto/create-short-term-data.dto.ts
@@ -1,16 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsInt, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class CreateShortTermDataDto {
   @ApiProperty({ example: 220 })
   @IsNotEmpty()
+  @IsNumber()
   voltage: number;
 
   @ApiProperty({ example: 10 })
   @IsNotEmpty()
+  @IsNumber()
   power: number;
 
   @ApiProperty({ example: 1 })
   @IsNotEmpty()
+  @IsInt()
   line: number;
 }
